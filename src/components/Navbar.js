cff13ed8e7d0aa03e1c55fe57377230ae95487a2
DeanDal1000 <dalzelldean@gmail.com>
2021-02-26 12:31:41 +0000

    First Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Nav, NavContainer } from './Navbar.elements';
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+        <NavContainer>yyy</NavContainer>
+      </Nav>
+    </div>
+  );
+};
+
+export default Navbar;
